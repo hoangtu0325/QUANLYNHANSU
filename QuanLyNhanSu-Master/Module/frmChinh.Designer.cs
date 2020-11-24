d19a88dde60acfb2a632099d004bb22da43f1a3f
Phamhung <Phamhung@INDIE>
2020-11-24 12:57:03 +0700

    ấ

--- QuanLyNhanSu-Master/Module/frmChinh.Designer.cs
+++ QuanLyNhanSu-Master/Module/frmChinh.Designer.cs
@@ -36,18 +36,18 @@
             this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
             this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
             this.btnExit = new Bunifu.Framework.UI.BunifuImageButton();
-            this.iconPictureBox3 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconSearch = new FontAwesome.Sharp.IconPictureBox();
             this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
             this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
-            this.bunifuImageButton3 = new Bunifu.Framework.UI.BunifuImageButton();
-            this.bunifuMaterialTextbox1 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.LogoACB = new Bunifu.Framework.UI.BunifuImageButton();
+            this.txtSearch = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.PanelTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconSearch)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoACB)).BeginInit();
             this.SuspendLayout();
             // 
             // bunifuElipse1
@@ -60,11 +60,11 @@
             this.PanelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(65)))), ((int)(((byte)(155)))));
             this.PanelTop.Controls.Add(this.bunifuImageButton1);
             this.PanelTop.Controls.Add(this.btnExit);
-            this.PanelTop.Controls.Add(this.iconPictureBox3);
+            this.PanelTop.Controls.Add(this.iconSearch);
             this.PanelTop.Controls.Add(this.iconPictureBox2);
             this.PanelTop.Controls.Add(this.iconPictureBox1);
-            this.PanelTop.Controls.Add(this.bunifuImageButton3);
-            this.PanelTop.Controls.Add(this.bunifuMaterialTextbox1);
+            this.PanelTop.Controls.Add(this.LogoACB);
+            this.PanelTop.Controls.Add(this.txtSearch);
             this.PanelTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.PanelTop.Location = new System.Drawing.Point(0, 0);
             this.PanelTop.Margin = new System.Windows.Forms.Padding(2);
@@ -117,20 +117,20 @@
             this.btnExit.Zoom = 10;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
             // 
-            // iconPictureBox3
+            // iconSearch
             // 
-            this.iconPictureBox3.BackColor = System.Drawing.Color.White;
-            this.iconPictureBox3.ForeColor = System.Drawing.Color.Black;
-            this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.Sistrix;
-            this.iconPictureBox3.IconColor = System.Drawing.Color.Black;
-            this.iconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox3.IconSize = 23;
-            this.iconPictureBox3.Location = new System.Drawing.Point(159, 10);
-            this.iconPictureBox3.Name = "iconPictureBox3";
-            this.iconPictureBox3.Padding = new System.Windows.Forms.Padding(2, 3, 2, 2);
-            this.iconPictureBox3.Size = new System.Drawing.Size(25, 23);
-            this.iconPictureBox3.TabIndex = 2;
-            this.iconPictureBox3.TabStop = false;
+            this.iconSearch.BackColor = System.Drawing.Color.White;
+            this.iconSearch.ForeColor = System.Drawing.Color.Black;
+            this.iconSearch.IconChar = FontAwesome.Sharp.IconChar.Sistrix;
+            this.iconSearch.IconColor = System.Drawing.Color.Black;
+            this.iconSearch.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconSearch.IconSize = 23;
+            this.iconSearch.Location = new System.Drawing.Point(159, 10);
+            this.iconSearch.Name = "iconSearch";
+            this.iconSearch.Padding = new System.Windows.Forms.Padding(2, 3, 2, 2);
+            this.iconSearch.Size = new System.Drawing.Size(25, 23);
+            this.iconSearch.TabIndex = 2;
+            this.iconSearch.TabStop = false;
             // 
             // iconPictureBox2
             // 
@@ -157,42 +157,41 @@
             this.iconPictureBox1.TabIndex = 2;
             this.iconPictureBox1.TabStop = false;
             // 
-            // bunifuImageButton3
+            // LogoACB
             // 
-            this.bunifuImageButton3.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton3.Image")));
-            this.bunifuImageButton3.ImageActive = null;
-            this.bunifuImageButton3.Location = new System.Drawing.Point(20, 2);
-            this.bunifuImageButton3.Margin = new System.Windows.Forms.Padding(2);
-            this.bunifuImageButton3.Name = "bunifuImageButton3";
-            this.bunifuImageButton3.Size = new System.Drawing.Size(61, 38);
-            this.bunifuImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.bunifuImageButton3.TabIndex = 19;
-            this.bunifuImageButton3.TabStop = false;
-            this.bunifuImageButton3.Zoom = 10;
+            this.LogoACB.BackColor = System.Drawing.Color.Transparent;
+            this.LogoACB.Image = ((System.Drawing.Image)(resources.GetObject("LogoACB.Image")));
+            this.LogoACB.ImageActive = null;
+            this.LogoACB.Location = new System.Drawing.Point(20, 2);
+            this.LogoACB.Margin = new System.Windows.Forms.Padding(2);
+            this.LogoACB.Name = "LogoACB";
+            this.LogoACB.Size = new System.Drawing.Size(61, 38);
+            this.LogoACB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoACB.TabIndex = 19;
+            this.LogoACB.TabStop = false;
+            this.LogoACB.Zoom = 10;
             // 
-            // bunifuMaterialTextbox1
+            // txtSearch
             // 
-            this.bunifuMaterialTextbox1.BackColor = System.Drawing.Color.White;
-            this.bunifuMaterialTextbox1.BackgroundImage = global::QuanLyNhanSu_Master.Properties.Resources.white;
-            this.bunifuMaterialTextbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.bunifuMaterialTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.bunifuMaterialTextbox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bunifuMaterialTextbox1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.bunifuMaterialTextbox1.HintForeColor = System.Drawing.Color.Empty;
-            this.bunifuMaterialTextbox1.HintText = "Tìm kiếm";
-            this.bunifuMaterialTextbox1.isPassword = false;
-            this.bunifuMaterialTextbox1.LineFocusedColor = System.Drawing.Color.White;
-            this.bunifuMaterialTextbox1.LineIdleColor = System.Drawing.Color.White;
-            this.bunifuMaterialTextbox1.LineMouseHoverColor = System.Drawing.Color.White;
-            this.bunifuMaterialTextbox1.LineThickness = 4;
-            this.bunifuMaterialTextbox1.Location = new System.Drawing.Point(150, 9);
-            this.bunifuMaterialTextbox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.bunifuMaterialTextbox1.Name = "bunifuMaterialTextbox1";
-            this.bunifuMaterialTextbox1.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
-            this.bunifuMaterialTextbox1.Size = new System.Drawing.Size(265, 24);
-            this.bunifuMaterialTextbox1.TabIndex = 21;
-            this.bunifuMaterialTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtSearch.BackColor = System.Drawing.Color.White;
+            this.txtSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtSearch.HintForeColor = System.Drawing.Color.Empty;
+            this.txtSearch.HintText = "Tìm kiếm";
+            this.txtSearch.isPassword = false;
+            this.txtSearch.LineFocusedColor = System.Drawing.Color.White;
+            this.txtSearch.LineIdleColor = System.Drawing.Color.White;
+            this.txtSearch.LineMouseHoverColor = System.Drawing.Color.White;
+            this.txtSearch.LineThickness = 4;
+            this.txtSearch.Location = new System.Drawing.Point(150, 9);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.txtSearch.Size = new System.Drawing.Size(265, 24);
+            this.txtSearch.TabIndex = 21;
+            this.txtSearch.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // frmChinh
             // 
@@ -211,10 +210,10 @@
             this.PanelTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconSearch)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoACB)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -225,10 +224,10 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel PanelTop;
         private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
-        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton3;
-        private Bunifu.Framework.UI.BunifuMaterialTextbox bunifuMaterialTextbox1;
+        private Bunifu.Framework.UI.BunifuImageButton LogoACB;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtSearch;
         private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
-        private FontAwesome.Sharp.IconPictureBox iconPictureBox3;
+        private FontAwesome.Sharp.IconPictureBox iconSearch;
         private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
         private Bunifu.Framework.UI.BunifuImageButton btnExit;
         private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
